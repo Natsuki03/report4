c9572a302589bc35436945521e16912f35155434
Natsuki Oshiro <e195767@ie.u-ryukyu.ac.jp>
2019-12-08 13:03:38 +0900

    step3

--- src/main/report4.java
+++ src/main/report4.java
@@ -1,12 +1,7 @@
 public class report4 {
     public static void main(String[] args){
-        String str = null;
-        try {
-            int num = str.length();
-            System.out.println(num);
-        }catch(NullPointerException e){
-            System.out.println("NullPointerExceptionが発生しました");
-            System.out.println(e.getMessage());
-        }
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
+        
     }
 }
